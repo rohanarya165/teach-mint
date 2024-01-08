e0b3fd39c7b674c8147e9b8c91ddb452396ca04e
rohan arya <rohan.a@mantralabsglobal.com>
2024-01-08 20:34:48 +0530

    Last Changes

--- src/components/ClockFunction.jsx
+++ src/components/ClockFunction.jsx
@@ -1,47 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function ClockFunction(props) {
-    const [time, setTime] = useState(new Date());
-    const [isPaused, setIsPaused] = useState(false);
-    const [pauseTime, setPauseTime] = useState(null);
-    console.log("time prop", props.selectedTime)
+  const [time, setTime] = useState(new Date(props.selectedTime));
+  const [isPaused, setIsPaused] = useState(false);
+  const [pauseTime, setPauseTime] = useState(null);
 
-    useEffect(() => {
-        const update = setInterval(() => {
-            if (!isPaused) {
-                setTime(new Date());
-            }
-        }, 1000);
+  useEffect(() => {
+    const update = setInterval(() => {
+      if (!isPaused) {
+        setTime(new Date());
+      }
+    }, 1000);
 
-        return () => clearInterval(update);
-    }, [isPaused]);
+    return () => clearInterval(update);
+  }, [isPaused]);
 
-    const handlePauseResumeClick = () => {
-        if (isPaused) {
-            // Resume the clock
-            setIsPaused(false);
-            const elapsedMilliseconds = new Date() - pauseTime;
-            setTime(new Date(time.getTime() + elapsedMilliseconds));
-            setPauseTime(null);
-        } else {
-            // Pause the clock
-            setIsPaused(true);
-            setPauseTime(new Date());
-        }
-    };
+  const handlePauseResumeClick = () => {
+    if (isPaused) {
+      // Resume the clock
+      setIsPaused(false);
+      const elapsedMilliseconds = new Date() - pauseTime;
+      setTime(new Date(time.getTime() + elapsedMilliseconds));
+      setPauseTime(null);
+    } else {
+      // Pause the clock
+      setIsPaused(true);
+      setPauseTime(new Date());
+    }
+  };
 
-    return (
-        <div style={{display:"flex", gap:"50px"}}>
-            <div className='clockDesign'>
-                {time.toLocaleTimeString()}
-            </div>
-            <div>
-                <button onClick={handlePauseResumeClick} className='dropDownCss'>
-                    {isPaused ? 'Resume' : 'Pause'}
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", gap: "50px" }}>
+      <div className="clockDesign">{time.toLocaleTimeString()}</div>
+      <div>
+        <button onClick={handlePauseResumeClick} className="dropDownCss">
+          {isPaused ? "Resume" : "Pause"}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ClockFunction;
--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -1,83 +1,111 @@
-// src/components/UserDetail.js
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import ClockFunction from "./ClockFunction";
-import {convertTimeFormat} from "./timeConverter"
+import { convertTimeFormat } from "./timeConverter";
 
 const UserDetail = () => {
   const { userId } = useParams();
   const [user, setUser] = useState({});
   const [posts, setPosts] = useState([]);
   const [contries, setContries] = useState([]);
-  const [selectedCountry,setSelectedCountry] = useState([]);
-  const [selectedTime, setselectedTime] = useState("")
+  const [selectedCountry, setSelectedCountry] = useState([]);
+  const [selectedTime, setselectedTime] = useState("");
+  const nevigate = useNavigate();
 
   useEffect(() => {
     const fetchUser = async () => {
-      const userResponse = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${userId}`
-      );
-      setUser(userResponse.data);
+      axios
+        .get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        .then((res) => {
+          setUser(res.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
 
-      const postResponse = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
-      );
-      setPosts(postResponse.data);
+      axios
+        .get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+        .then((res) => {
+          setPosts(res.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     };
 
-    axios.get("http://worldtimeapi.org/api/timezone").then((res) => {
-      setContries(res.data);
-    }).catch(function (error) {
-      console.log(error);
-    });
+    axios
+      .get("http://worldtimeapi.org/api/timezone")
+      .then((res) => {
+        setContries(res.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
     fetchUser();
   }, [userId]);
 
   let selectCountryFn = (e) => {
-    setSelectedCountry(e.target.value)
-    axios.get(`http://worldtimeapi.org/api/timezone/${selectedCountry}`).then((res) => {
-      let time =  convertTimeFormat(res.data.datetime)   
-      console.log("res.data.datetime", time)
-         setselectedTime(time)
-    }).catch(function (error) {
-      console.log(error);
-    });
-  }
+    setSelectedCountry(e.target.value);
+    axios
+      .get(`http://worldtimeapi.org/api/timezone/${selectedCountry}`)
+      .then((res) => {
+        let time = convertTimeFormat(res.data.datetime);
+        setselectedTime(time);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="container">
-      <Link to="/" className="back-link">
-        Back to User Directory
-      </Link>
-
-      <h1>{user.name}'s Profile</h1>
+      <div style={{ display: "flex", gap: "20px", marginBottom: "20px" }}>
+        <button onClick={() => nevigate("/")} className="button-style">
+          Back
+        </button>
+        <h1>{user.name}'s Profile</h1>
+      </div>
       <div className="clock-section">
         <h2>Current Time</h2>
         <select className="dropDownCss" onClick={selectCountryFn}>
           {contries.map((country) => {
-            return <option value={country} >{country}</option>;
+            return <option value={country}>{country}</option>;
           })}
         </select>
-        <ClockFunction selectedTime={selectedTime}/>
+        <ClockFunction selectedTime={selectedTime} />
       </div>
       <div className="user-details-section">
         <div>
           <h2>Name</h2>
           <p>{user.name}</p>
-          <h2>Username</h2>
-          <p>{user.username}</p>
-          <h2>Catch Phrase</h2>
-          <p>{user.company?.catchPhrase}</p>
+          <div style={{ display: "flex", gap: "40px", marginBottom: "20px" }}>
+            <div>
+              <h2>Username</h2>
+              <p>{user.username}</p>
+            </div>
+            <hr />
+            <div>
+              <h2>Catch Phrase</h2>
+              <p>{user.company?.catchPhrase}</p>
+            </div>
+          </div>
         </div>
         <div>
           <h2>Address</h2>
           <p>{`${user.address?.city}, ${user.address?.street}, ${user.address?.suite}`}</p>
-          <h2>Email</h2>
-          <p>{user.email}</p>
-          <h2>Phone</h2>
-          <p>{user.phone}</p>
+          <div style={{ display: "flex", gap: "40px", marginBottom: "20px" }}>
+            <div>
+              <h2>Email</h2>
+              <p>{user.email}</p>
+            </div>
+            <hr />
+            <div>
+              <h2>Phone</h2>
+              <p>{user.phone}</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="posts-section">
